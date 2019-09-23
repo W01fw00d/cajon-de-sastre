7bc11b0b78ce307b61824b17db9843320f93108a
W01fw00d <gabrm_91@hotmail.com>
2019-09-23 20:13:57 +0200

    Add min_diff utils

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cajon-de-sastre
 Some useful utils functions for Python
+
+
+## Free Fun Spanish Tip
+'Cajón de Sastre' means 'Tailor's Drawer', and it's a Spanish expression for a group of diverse and unordered things.
+It's specially interesting also that the word 'Desastre' means disaster in Spanish.
+That's why this expression is comonly misspelled as 'Cajón Desastre'
---
+++ min_diff.py
@@ -0,0 +1,37 @@
+import sys
+import math
+
+def getInput(n_lines):
+    print("Input lines: ", file=sys.stderr)
+    input_lines = []
+    for line in range(n_lines):
+        new_line = int(input())
+        input_lines.append(new_line)
+        print(new_line, file=sys.stderr)
+
+    return input_lines
+
+def getMinimumDifference(array):
+    array.sort()
+    
+    min_item_diff = None
+    prev_item = array[0]
+    for item in array[1 : len(array)]:
+        item_diff = abs(prev_item - item)
+        prev_item = item
+        if min_item_diff is None or min_item_diff > item_diff:
+            min_item_diff = item_diff
+            if min_item_diff == 0:
+                break
+       
+    return min_item_diff
+
+
+n_lines = int(input())
+print("Number of items: ", n_lines, file=sys.stderr)
+
+print(
+    getMinimumDifference(
+        getInput(n_lines)
+    )
+)
